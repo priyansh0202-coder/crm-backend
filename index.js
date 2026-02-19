6d500bf39d0e97f1d440dabc5c898deb142f1d6b
priyanshinfusyx <infusyx.priyansh@gmail.com>
2026-02-19 15:06:32 +0530

    feat: Update CORS configuration to allow all origins and credentials.

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
 // app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
